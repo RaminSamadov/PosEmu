54ac3231af262186b6e7b3b7a7e21785af7fd157
Juris Lacis <jurislacis@Juriss-MacBook-Pro.local>
2019-05-08 18:10:09 +0400

    first

---
+++ src/az/pashabank/posemu/Constants.java
@@ -0,0 +1,19 @@
+package az.pashabank.posemu;
+
+class Constants {
+    
+    static final String PARAM_ACQ_HOST_IP_ADDRESS = "ACQ_HOST_IP_ADDRESS";
+    static final String PARAM_ACQ_HOST_PORT = "ACQ_HOST_PORT";
+    static final String PARAM_ACQ_HOST_TIMEOUT = "ACQ_HOST_TIMEOUT";
+    static final String PARAM_TERMINAL_ID = "TERMINAL_ID";
+    static final String PARAM_TERMINAL_MERCHANT_ID = "TERMINAL_MERCHANT_ID";
+    static final String PARAM_PIN_KEY = "PIN_KEY";
+    static final String PARAM_PIN_KEY_IS_USED = "PIN_KEY_IS_USED";
+    static final String PARAM_ENCRYPTION_KEY = "ENCRYPTION_KEY";
+    static final String PARAM_ENCRYPTION_KEY_IS_USED = "ENCRYPTION_KEY_IS_USED";
+    static final String PARAM_MAC_KEY = "MAC_KEY";
+    static final String PARAM_MAC_KEY_IS_USED = "MAC_KEY_IS_USED";
+    static final String PARAM_MASTER_KEY = "MASTER_KEY";
+    static final String PARAM_MASTER_KEY_IS_USED = "MASTER_KEY_IS_USED";
+    
+}
